31446f8a3509938fedad04d56c11dfb4b330dd6a
Dorian <dorian.chantrel@hotmail.fr>
2025-09-23 20:37:41 +0200

    Test cookie open bar

--- app/src/app/interceptors/bearer-token.interceptor.ts
+++ app/src/app/interceptors/bearer-token.interceptor.ts
@@ -5,6 +5,7 @@ import { CookieService } from 'ngx-cookie-service';
 export const BearerTokenInterceptor: HttpInterceptorFn = (req, next) => {
   const cookieService = inject(CookieService);
   const accessToken = cookieService.get('accessToken');
+  console.log('Access Token:', accessToken); // Log the access token to the console
 
   if (accessToken) {
     const cloned = req.clone({
